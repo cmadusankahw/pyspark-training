db696a740ee1c7ee9610253049cb67879459ca9a
cmadusankahw <madushan_im16046@stu.kln.ac.lk>
2020-09-10 09:37:07 +0530

    pyspark RDD training exercises

---
+++ com/DSE/pyspark-training/exercise02/WorkingWithRDDs.py
@@ -0,0 +1,73 @@
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+from matplotlib import pyplot as plt
+from pandas.plotting import scatter_matrix
+import pandas as pd
+
+# create spark session
+sc = SparkContext("local", "rdd-training")
+
+################################################################
+
+# Exercise 1: create RDD with parallelizing a list
+words = sc.parallelize(
+    ["scala",
+     "java",
+     "hadoop",
+     "spark",
+     "akka",
+     "spark vs hadoop",
+     "pyspark",
+     "pyspark and spark"]
+)
+
+print(words.take(3))
+print(words.count())
+
+
+# Print element with foreach
+def f(x): print(x)
+
+
+# in the foreach loop a function is called on every element in rdd
+words.foreach(lambda x: f(x))
+# or more simpler
+words.foreach(f)
+
+# applying filter transformation
+words_filter = words.filter(lambda y: 'spark' in y)
+filtered = words_filter.collect()
+print("Filtered: " + filtered)
+
+# applying map transformation
+words_map = words.map(lambda x: (x, 1))
+mapped = words_map.collect()
+print(" Mapped Key-Value pairs: " + mapped)
+
+##################################################################
+
+# Exercise 2 : Create RDD from a file input
+# Read raw text to RDD
+lines = sc.textFile('in/airports.text')
+
+# formatting & splitting values in each row
+airportRdd = lines.map(lambda x: (x.replace('"', '').split(",")))
+
+print(type(airportRdd))
+print(airportRdd.take(2))
+
+##################################################################
+
+# Exercise 3: WordCount in pyspark with reduce & GroupByKey
+# Read RDD from a text file
+wc_lines = sc.textFile('in/word_count.text')
+
+# flatten the String regular RDD into a list RDD of words
+words = wc_lines.flatMap(lambda line: line.split(" "))
+
+# count words to a map with countByValue
+wordCounts = words.countByValue()
+
+# count words with reduce() and groupByKey() transformations
+
+wordCounts.foreach(f)
